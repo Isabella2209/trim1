81b0c74a2d5364b571ca151f1f3df74b6dd0ee72
Isabella2209 <162451640+Isabella2209@users.noreply.github.com>
2025-03-10 08:25:50 -0300

    Update atividade1.js

--- atividade1.js
+++ atividade1.js
@@ -1,9 +1,6 @@
-let av1 = 10.0
-let av2 = 70.0
-let av3 = 40.0
-const media = ( av1 + av2 + av3 )/3
-if(media>=6.0){
-    console.log("aprovado");
-}else{
-    console.log("reprovado");
-}
+// exercício 1:
+const peso = 58;
+const agua_ml = peso * 58;
+const agua_l = agua_ml/1000;
+console.log("a quantidade ideal de agua é" + agua_l + "litros")
+
