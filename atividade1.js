68f2502d894d9c76bbaa9273e03bfbbbeea55c46
Isabella2209 <162451640+Isabella2209@users.noreply.github.com>
2025-03-10 08:23:21 -0300

    Update atividade1.js

--- atividade1.js
+++ atividade1.js
@@ -1,7 +1,9 @@
-let av1 = 10.0 
-let av2 = 7.0
-let av3 = 6.0 
-
-let sub = av1 - av2 - av3
-
-console.log("subtracao = " + sub)
+let av1 = 10.0
+let av2 = 70.0
+let av3 = 40.0
+const media = ( av1 + av2 + av3 )/3
+if(media>=6.0){
+    console.log("aprovado");
+}else{
+    console.log("reprovado");
+}
