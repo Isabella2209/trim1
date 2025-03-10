ac203a24e14b4f4c3dd5443a0b969fb53066d571
Isabella2209 <162451640+Isabella2209@users.noreply.github.com>
2025-03-10 08:44:49 -0300

    Update atividade2.js

--- atividade2.js
+++ atividade2.js
@@ -1,10 +1,11 @@
 // exercício 2:
 let av1 = 10.0
-let av2 = 70.0
-let av3 = 40.0
+let av2 = 7
+let av3 = 4
 const media = ( av1 + av2 + av3 )/3
 if(media>=6.0){
     console.log("aprovado");
 }else{
     console.log("reprovado");
 }
+console.log("a media é " + media)
